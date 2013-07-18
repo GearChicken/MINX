dde851c6fc6d59ca9433a5002361a34b5187faa0
Benjamin <brpylko@gmail.com>
2013-07-18 12:12:13 -0400

    Added headers tobe used in a project

---
+++ src/MINX.h
@@ -0,0 +1,27 @@
+/*
+    MINX - A C++ Graphics and Input Wrapper Library
+    Copyright (C) 2013  MINX Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	*/
+#ifndef MINX_H_
+#define MINX_H_
+#include "Game.h" //some of this is just for organization, since many .h files include other ones anyway
+#include "GameComponent.h"
+#include "GameTime.h"
+#include "Point.h"
+#include "Vector2.h"
+#include "Matrix4x4.h"
+#endif
