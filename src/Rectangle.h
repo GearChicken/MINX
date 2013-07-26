d088b107f845c1efad9f391df8c911d1b98f071c
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-07-25 23:17:49 -0400

    Added Rectangle Struct

---
+++ src/Rectangle.h
@@ -0,0 +1,35 @@
+/*
+    MINX - A C++ Graphics and Input Wrapper Library
+    Copyright (C) 2013  MINX Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	*/
+#ifndef RECTANGLE_H_
+#define RECTANGLE_H_
+namespace MINX
+{
+	struct Rectangle
+	{
+		Rectangle(int x, int y, int width, int height);
+		bool operator==(const Rectangle& compareTo);
+		bool operator!=(const Rectangle& compareTo);
+		int area();
+		int X;
+		int Y;
+		int Width;
+		int Height;
+	};
+}
+#endif
