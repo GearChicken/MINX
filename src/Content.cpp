4881c944aa4afc8143a3ee16ce8e3767344af685
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-07-29 00:17:21 -0400

    Added Content class and some stuff concerning loading fonts

---
+++ src/Content.cpp
@@ -0,0 +1,34 @@
+/*
+    MINX - A C++ Graphics and Input Wrapper Library
+    Copyright (C) 2013  MINX Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	*/
+
+#include "GameTime.h"
+using namespace MINX;
+
+Content::Content()
+{
+}
+
+Texture2D* Content::loadTexture(std::string)
+{
+	
+}
+TTF_Font* Content::loadTTFFont(std::string, int size)
+{
+	TTF_OpenFont(string.c_str(), size);
+}
---
+++ src/Content.h
@@ -0,0 +1,37 @@
+/*
+    MINX - A C++ Graphics and Input Wrapper Library
+    Copyright (C) 2013  MINX Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	*/
+
+#ifndef CONTENT_H_
+#define CONTENT_H_
+#include "SDL/SDL.h"
+#include "Graphics/Texture2D.h"
+#include <string>
+namespace MINX
+{
+	class Content
+	{
+		public:
+			Content();
+			Texture2D* loadTexture(std::string);
+			vector<Texture2D*>* textures;
+			TTF_Font* loadTTFFont(std::string);
+			vector<TTF_Font*>* TTFFonts;
+	};
+}
+#endif
