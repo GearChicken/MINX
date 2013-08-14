21e142f259b8f0fc04d00870624277aba52074c4
Benjamin <brpylko@gmail.com>
2013-08-14 11:04:31 -0400

    Added more functionality to AudioClip

--- src/Audio/AudioClip.cpp
+++ src/Audio/AudioClip.cpp
@@ -46,6 +46,9 @@ AudioClip::AudioClip(string filename, bool autoplay, bool loop)
 	{
 		Mix_PlayChannel(mix_channel, audiodata, loop ? -1 : 0);
 	}
+	chunkAngle = 0;
+	chunkDistance = 0;
+	chunkVolume = MIX_MAX_VOLUME;
 }
 #ifndef _WIN32
 AudioClip::AudioClip(string filename) : AudioClip(filename,0,0){}
@@ -96,7 +99,23 @@ void AudioClip::setVolume(float volume)
 
 void AudioClip::setPosition(Sint16 angle, Uint8 distance)
 {
-	Mix_SetPosition(mix_channel,angle,distance);
+	chunkAngle = angle;
+	chunkDistance = distance;
+	chunkVolume = 128.0/(double(distance+1)*double(distance+1));
+	Mix_SetPosition(mix_channel,angle,0);
+	setVolume(chunkVolume);
+}
+
+void AudioClip::headPhoneMode()
+{
+	chunkAngle %= 360;
+	else if((chunkAngle >= 180 && chunkAngle < 360) || (chunkAngle <= -180 && > -360))
+	{
+		chunkAngle -= 360;
+	}
+	chunkAngle %= 90;
+	chunkAngle /= 2;
+	setPosition(chunkAngle,chunkDistance);
 }
 
 void AudioClip::disable3DAudio()
