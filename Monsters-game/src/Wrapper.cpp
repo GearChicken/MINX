ccac19cc275d58788c3fd8fbf24284a9658bf3af
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-10-13 00:26:00 -0400

    Fixed Some Dependency Issues in MINXTestProject Created MINX_Monsters a inheritance based tech demo!

---
+++ Monsters-game/src/Wrapper.cpp
@@ -0,0 +1,46 @@
+/*
+MINX_Monsters - A C++ Game Based off the
+Copyright (C) 2013  MINX Team
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include "Wrapper.h"
+using namespace MINX_monsters;
+Wrapper::Wrapper(Vector2* position, Graphics::Texture2D* texture) : Monster(position, texture, new Graphics::Color(0.0f,0.0f,255.0f))
+{
+	velocity->X = -1;
+	velocity->Y = -1;
+}
+void Wrapper::Update(GameTime* gameTime, Input::Keyboard* keyboard)
+{
+	position->X += velocity->X;
+	position->Y += velocity->Y;
+	if (position->X < 0)
+	{
+		position->X = Graphics::GameWindow::width - texture->width;
+	}
+	if (position->Y < 0)
+	{
+		position->Y = Graphics::GameWindow::height - texture->height;
+	}
+	if (position->X > Graphics::GameWindow::width - texture->width)
+	{
+		position->X = 0;
+	}
+	if (position->Y > Graphics::GameWindow::height - texture->height)
+	{
+		position->Y = 0;
+	}
+}
