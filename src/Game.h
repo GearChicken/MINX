9a2498e4e1e95587d79f8922e5a23d4e3f48791a
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-11-12 00:35:28 -0500

    Got Orthographic Projection Working!

--- src/Game.h
+++ src/Game.h
@@ -1,21 +1,21 @@
 /*
-    MINX - A C++ Graphics and Input Wrapper Library
-    Copyright (C) 2013  MINX Team
+MINX - A C++ Graphics and Input Wrapper Library
+Copyright (C) 2013  MINX Team
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	*/
+*/
 #ifndef GAME_H_
 #define GAME_H_
 #define MINX_DEBUG
@@ -40,67 +40,78 @@ using namespace std;
 namespace MINX
 {
 	/** The class that handles everything.
-	 * When using the library, your code will probably construct, Initialize() and Run() your subclass of Game.
-	 */
+	* When using the library, your code will probably construct, Initialize() and Run() your subclass of Game.
+	*/
 	class Game 
 	{
-		public:
-			/** The constructor, which handles some of the initialization code for the Game.
-			 */
-			Game();
-			/** This handles the rest of the initialization code for the game.
-			 */
-			virtual void Initialize();
-			/** Loads content used by the game.
-			 */
-			virtual void LoadContent();
-			/** Updates the game state.
-			 * Should be extended when you create a subclass of Game.
-			 * @param gameTime the GameTime to use when updating.
-			 */
-			 virtual void Update(GameTime * gameTime);
-			/** Unloads content used by the game.
-			 */
-			 virtual void UnloadContent();
-			/** Draws the state of the game every frame.
-			 * This method is called by a different thread than Update(), so it is ok to use sleeps in Update() and not worry about freezing the drawing, or vice versa.
-			 * @param gameTime the GameTime to use when drawing.
-			 */
-			 virtual void Draw(GameTime * gameTime);
-			/** Handles Update()ing, Draw()ing, and event handling.
-			 */
-			 void Run();
-			/** A pointer to the window used by the game.
-			 */
-			Graphics::GameWindow* gameWindow;
-			/** A pointer to a vector of pointers to the GameComponents used in the game.
-			 */
-			vector<GameComponent*> * Components;
-			vector<EventHandler*> * eventHandlers;
-			/** Returns a pointer to the GameTime being used by the game.
-			 * Currently GameTime is pretty much just stubbed.
-			 */
-			GameTime * getGameTime();
-			void setVideoOptions(int desiredFPS, int windowWidth, int windowHeight, int windowBPP, int windowFlags);
-			void setVideoOptions(int desiredFPS, int windowWidth, int windowHeight, int windowBPP, int windowFlags, char* windowTitle);
-			bool isRunning;
-			bool preventAutoQuitting;
-			int desiredFPS;
-			int windowWidth;
-			int windowHeight;
-			int windowBPP;
-			char* windowTitle;
-			int windowFlags;
-			GLuint shaderProgram;
-		private:
-			/** The object that will store the event received by SDL_PollEvent() each cycle.
-			 */
-			/** A pointer to the GameTime being used by the game.
-			 */
-			GameTime* gameTime;
-			/** A semaphore to protect the draw thread  reading invalid data
-			*/
-		protected:
+	public:
+		/** The constructor, which handles some of the initialization code for the Game.
+		*/
+		Game();
+		/** This handles the rest of the initialization code for the game.
+		*/
+		virtual void Initialize();
+		/** Loads content used by the game.
+		*/
+		virtual void LoadContent();
+		/** Updates the game state.
+		* Should be extended when you create a subclass of Game.
+		* @param gameTime the GameTime to use when updating.
+		*/
+		virtual void Update(GameTime * gameTime);
+		/** Unloads content used by the game.
+		*/
+		virtual void UnloadContent();
+		/** Draws the state of the game every frame.
+		* This method is called by a different thread than Update(), so it is ok to use sleeps in Update() and not worry about freezing the drawing, or vice versa.
+		* @param gameTime the GameTime to use when drawing.
+		*/
+		virtual void Draw(GameTime * gameTime);
+		/** Handles Update()ing, Draw()ing, and event handling.
+		*/
+		void Run();
+		/** A pointer to the window used by the game.
+		*/
+		Graphics::GameWindow* gameWindow;
+		/** A pointer to a vector of pointers to the GameComponents used in the game.
+		*/
+		vector<GameComponent*> * Components;
+		vector<EventHandler*> * eventHandlers;
+		/** Returns a pointer to the GameTime being used by the game.
+		* Currently GameTime is pretty much just stubbed.
+		*/
+		GameTime * getGameTime();
+		/** Set the video options when creating a new GameWindow
+		*	@param windowWidth The Width of the GameWindow
+		*	@param windowHeight The Height of the GameWindow
+		*	@param windowBPP The BitsPerPixel of the GameWindow
+		*	@param windowFlags The flags to instantiate the GameWindow with
+		*/
+		void setVideoOptions(int windowWidth, int windowHeight, int windowBPP, int windowFlags);
+		
+		/** Set the video options when creating a new GameWindow
+		*	@param windowWidth The Width of the GameWindow
+		*	@param windowHeight The Height of the GameWindow
+		*	@param windowBPP The BitsPerPixel of the GameWindow
+		*	@param windowFlags The flags to instantiate the GameWindow with
+		*	@param windowTitle The title of the GameWindow
+		*/
+		void setVideoOptions(int windowWidth, int windowHeight, int windowBPP, int windowFlags, char* windowTitle);
+		/** Is the Game Running, or should it close?
+		*/
+		bool isRunning;
+	protected:
+		/** The Shader Program that OpenGl will use to draw to the screen
+		GLuint shaderProgram;
+	private:
+		/** A pointer to the GameTime being used by the game.
+		*/
+		GameTime* gameTime;
+		int windowWidth;
+		int windowHeight;
+		int windowBPP;
+		char* windowTitle;
+		int windowFlags;
 	};
 }
 #endif
