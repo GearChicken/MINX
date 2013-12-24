18f99ba8305d2782c233f5c6fc2016b7722ef60a
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-12-24 00:53:38 -0500

    Fixed issue where resizing the GameWindow would mess up the screen drawing bounds

--- src/Graphics/GameWindow.cpp
+++ src/Graphics/GameWindow.cpp
@@ -1,27 +1,33 @@
 /*
-    MINX - A C++ Graphics and Input Wrapper Library
-    Copyright (C) 2013  MINX Team
+MINX - A C++ Graphics and Input Wrapper Library
+Copyright (C) 2013  MINX Team
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	*/
-	
+*/
+
 #include "GameWindow.h"
 
 using namespace MINX::Graphics;
 int GameWindow::width=0;
 int GameWindow::height=0;
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+	glfwSetWindowSize(window, GameWindow::width, GameWindow::height);
+}
+
 GameWindow::GameWindow(int width, int height, int bpp, int flags)
 {
 	GameWindow::width = width;
@@ -29,6 +35,7 @@ GameWindow::GameWindow(int width, int height, int bpp, int flags)
 	bpp = bpp;
 	flags = flags;
 	window = glfwCreateWindow(width, height, "MINX Window", NULL, NULL);
+	glfwSetWindowSizeCallback(window, windowResizeCallback);
 }
 GameWindow::GameWindow(int width, int height, int bpp, int flags, char* title)
 {
@@ -37,4 +44,5 @@ GameWindow::GameWindow(int width, int height, int bpp, int flags, char* title)
 	bpp = bpp;
 	flags = flags;
 	window = glfwCreateWindow(width, height, title, NULL, NULL);
+	glfwSetWindowSizeCallback(window, windowResizeCallback);
 }
