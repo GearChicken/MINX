daca2fe0cc6a81ed7a54ea5787e9e9842c726bcb
Benjamin <brpylko@gmail.com>
2014-01-16 21:51:16 -0500

    Changed Vector2 to use doubles instead of floats and added an additional constructor

--- src/Vector2.h
+++ src/Vector2.h
@@ -26,76 +26,100 @@ namespace MINX
 	{
 		/** Initializes the Vector2 with a value of (0, 0)
 		 */
-			Vector2();
+		Vector2();
+		
 		/** Initializes the Vector2 with an x and a y component.
 		 * @param x The x component of the Vector2.
 		 * @param y The y component of the Vector2.
 		 */
-			Vector2(float x, float y);
+		Vector2(double x, double y);
+		
+		/** Initializes the Vector2 with a magnitude and direction.
+		 * @param length The length of the Vector2.
+		 * @param angle The angle of the Vector2.
+		 * @param radians Whether the angle is in radians or degrees.
+		 */
+		Vector2(double length, double angle, bool radians);
+		
 		/** Adds 2 Vector2s.
 		 * @param addTo the Vector2 to add.
 		 */
-			Vector2 operator+(const Vector2& addTo);
+		Vector2 operator+(const Vector2& addTo);
+		
 		/** Adds 2 Vector2s.
 		 * @param addTo the Vector2 to add.
 		 */
-			void operator+=(const Vector2& addTo);
+		void operator+=(const Vector2& addTo);
+		
 		/** Subtracts a Vector2 from another.
 		 * @param subtractFrom the Vector2 to subtract.
 		 */
-			Vector2 operator-(const Vector2& subtractFrom);
+		Vector2 operator-(const Vector2& subtractFrom);
+		
 		/** Subtracts a Vector2 from another.
 		 * @param subtractFrom the Vector2 to subtract.
 		 */
-			void operator-=(const Vector2& subtractFrom);
+		void operator-=(const Vector2& subtractFrom);
+		
 		/** "cross multiplies" 2 Vector2s.
 		 * There are "'s there because you can't REALLY cross multiply a 2 dimensional vector.
 		 * @param multiplyBy the Vector2 to "cross multiply" by.
 		 */
-			Vector2 crossMultiply(const Vector2& multiplyBy);
+		double crossMultiply(const Vector2& multiplyBy);
+		
 		/** Dot multiplies 2 Vector2s.
 		 * @param multiplyBy the Vector2 to dot multiply by.
 		 */
-			float operator*(const Vector2& multiplyBy);
+		double operator*(const Vector2& multiplyBy);
+		
 		/** Multiplies a Vector2 by a scalar.
 		 * @param multiplyBy the scalar to multiply the length of the Vector2 by.
 		 */
-			Vector2 operator*(const float& multiplyBy);
+		Vector2 operator*(const double& multiplyBy);
+		
 		/** Multiplies a Vector2 by a scalar.
 		 * @param multiplyBy the scalar to multiply the length of the Vector2 by.
 		 */
-			void operator*=(const float& multiplyBy);
+		void operator*=(const double& multiplyBy);
+		
 		/** Negates a Vector2.
 		 */
-			Vector2 operator-()const;
+		Vector2 operator-()const;
+		
 		/** Tests equality between 2 Vector2s.
 		 * @param compareTo the Vector2 to test equality with.
 		 */
-			bool operator==(const Vector2& compareTo);
+		bool operator==(const Vector2& compareTo);
+		
 		/** Tests inequality between 2 Vector2s.
 		 * @param compareTo the Vector2 to test equality with.
 		 */
-			bool operator!=(const Vector2& compareTo);
+		bool operator!=(const Vector2& compareTo);
+		
 		/** Returns the length of the Vector2.
 		 */
-
-			float Length();
+		double Length();
+		
 		/** Returns the length of the Vector2 squared.
 		 * This is faster then squaring the result of length() because length() actually returns the square root of the mathematical result of the length squared.
 		 */
-			float LengthSquared();
+		double LengthSquared();
+		
 		/** Returns a Vector2 of length 1 but the same direction as the current Vector2.
 		 */
-			Vector2 Normalize();
+		Vector2 Normalize();
+		
 		/** Returns the direction of the vector in radians
 		*/
-			float Direction();
+		double Direction();
+		
 		/** The X component of the Vector2.
 		 */
-		float X;
+		double X;
+		
 		/** The Y component of the Vector2.
 		 */
-		float Y;
+		double Y;
 	};
 }
 #endif
