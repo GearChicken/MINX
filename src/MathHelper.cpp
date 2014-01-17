e6a95ff3ba962dca15f7991153bf0a7fa973b9ff
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-01-16 22:32:36 -0500

    Populated MathHelper with functions

--- src/MathHelper.cpp
+++ src/MathHelper.cpp
@@ -1,11 +1,177 @@
 #include "MathHelper.h"
 
 using namespace MINX;
-using namespace MINX::Math;
 
-int Math::Round(float x)
+//Constants
+
+double Math::PI = ::asin(-1.0);
+
+double Math::TAO = 2 * asin(-1.0);
+
+//Trig Functions
+
+double Math::sin(double radians)
 {
-	int wholeNumber = (int)x;
+	return ::sin(radians);
+}
+
+double Math::cos(double radians)
+{
+	return ::cos(radians);
+}
+
+double Math::tan(double radians)
+{
+	return ::tan(radians);
+}
+
+double Math::cot(double radians)
+{
+	return ::tan(1.0 / radians);
+}
+
+double Math::sec(double radians)
+{
+	return ::cos(1.0 / radians);
+}
+
+double Math::csc(double radians)
+{
+	return ::sin(1.0 / radians);
+}
+
+double Math::sinh(double radians)
+{
+	return ::sinh(radians);
+}
+
+double Math::cosh(double radians)
+{
+	return ::cosh(radians);
+}
+
+double Math::tanh(double radians)
+{
+	return ::tanh(radians);
+}
+
+double Math::coth(double radians)
+{
+	return ::tanh(1.0 / radians);
+}
+
+double Math::sech(double radians)
+{
+	return ::cosh(1.0 / radians);
+}
+
+double Math::csch(double radians)
+{
+	return ::sinh(1.0 / radians);
+}
+
+double Math::asin(double num)
+{
+	return ::asin(num);
+}
+
+double Math::acos(double num)
+{
+	return ::acos(num);
+}
+
+double Math::atan(double num)
+{
+	return ::atan(num);
+}
+
+double Math::atan2(double y, double x)
+{
+	return ::atan2(y, x);
+}
+
+double Math::acot(double num)
+{
+	return ::atan(1.0 / num);
+}
+
+double Math::acot2(double y, double x)
+{
+	return ::atan2(1.0 / y, 1.0 / x);
+}
+
+double Math::asec(double num)
+{
+	return ::acos(1.0 / num);
+}
+
+double Math::acsc(double num)
+{
+	return ::asin(1.0 / num);
+}
+
+double Math::degreesToRadians(double degrees)
+{
+	return degrees * Math::PI / 180.0;
+}
+
+double Math::radiansToDegrees(double radians)
+{
+	return radians * 180.0 / Math::PI;
+}
+
+//Logarithmic Functions
+
+double Math::ln(double num)
+{
+	return ::log(num);
+}
+
+double Math::log2(double num)
+{
+	return ::log(num) / ::log(2);
+}
+
+double Math::log10(double num)
+{
+	return ::log10(num);
+}
+
+double Math::log(double base, double num)
+{
+	return ::log(num) / ::log(base);
+}
+
+double Math::exp(double num)
+{
+	return ::exp(num);
+}
+
+double Math::pow(double num, double power)
+{
+	return ::pow(num, power);
+}
+
+//General Functions
+
+bool Math::sign(double num)
+{
+	return num >= 0;
+}
+
+double Math::abs(double num)
+{
+	return num >= 0 ? num : -num;
+}
+
+bool Math::parity(long num)
+{
+	return (num % 2) == 0;
+}
+
+double Math::round(double x)
+{
+	double wholeNumber = (int)x;
 	float dif = x - wholeNumber;
 	if(x >= 0.5f)
 	{
@@ -14,13 +180,17 @@ int Math::Round(float x)
 	return wholeNumber;
 }
 
-int Math::Round(double x)
+double Math::sqrt(double num)
 {
-	int wholeNumber = (int)x;
-	float dif = x - wholeNumber;
-	if(x >= 0.5f)
-	{
-		return wholeNumber + 1;
-	}
-	return wholeNumber;
+	return ::sqrt(num);
+}
+
+double Math::cbrt(double num)
+{
+	return pow(num, 1.0/3.0);
+}
+
+double Math::root(double degree, double num)
+{
+	return pow(num, 1.0/degree);
 }
--- src/MathHelper.h
+++ src/MathHelper.h
@@ -20,12 +20,56 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef MINX_MATH_H_
 #define MINX_MATH_H_
 
+#include <cmath>
+
 namespace MINX
 {
 	namespace Math
 	{
-		extern int Round(float x);
-		extern int Round(double x);
+		extern double PI;
+		extern double TAO;
+		/// Trig Functions
+		extern double sin(double radians);
+		extern double cos(double radians);
+		extern double tan(double radians);
+		extern double cot(double radians);
+		extern double sec(double radians);
+		extern double csc(double radians);
+		extern double sinh(double radians);
+		extern double cosh(double radians);
+		extern double tanh(double radians);
+		extern double coth(double radians);
+		extern double sech(double radians);
+		extern double csch(double radians);
+		extern double asin(double num);
+		extern double acos(double num);
+		extern double atan(double num);
+		extern double atan2(double y, double x);
+		extern double acot(double num);
+		extern double acot2(double y, double x);
+		extern double asec(double num);
+		extern double acsc(double num);
+
+		extern double degreesToRadians(double degrees);
+		extern double radiansToDegrees(double radians);
+
+		//Logarithmic Functions
+		extern double ln(double num);
+		extern double log2(double num);
+		extern double log10(double num);
+		extern double log(double base, double num);
+		extern double exp(double num);
+		extern double pow(double num, double power);
+
+		//General Functions
+		extern bool sign(double num);
+		extern double abs(double num);
+		extern bool parity(long num);
+		extern double round(double x);
+		extern double sqrt(double num);
+		extern double cbrt(double num);
+		extern double root(double degree, double num);
+
 	}
 }
 #endif
