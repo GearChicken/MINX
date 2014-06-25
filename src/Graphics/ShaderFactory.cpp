4375a58194b9310bdbe95967fcce7ee52317d9d7
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-06-25 18:58:28 -0400

    Added shader error logging

--- src/Graphics/ShaderFactory.cpp
+++ src/Graphics/ShaderFactory.cpp
@@ -82,75 +82,97 @@ ShaderFactory* ShaderFactory::GetInstance()
 
 std::string loadFileToString(char* fileLocation)
 {
-  //Load in the file
-  std::string content;
-  std::ifstream fileStream(fileLocation, std::ios::in);
+	//Load in the file
+	std::string content;
+	std::ifstream fileStream(fileLocation, std::ios::in);
 
-  //Make sure the file loaded in
-  if(!fileStream.is_open())
-  {
-    std::cout << "Could not read file: " << fileLocation << " File does NOT Exist" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-  std::string line = "";
+	//Make sure the file loaded in
+	if(!fileStream.is_open())
+	{
+		std::cout << "Could not read file: " << fileLocation << " File does NOT Exist" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::string line = "";
 
-  //Get every line of the file
-  while(!fileStream.eof())
-  {
-    std::getline(fileStream, line);
-    content.append(line + "\n");
-  }
+	//Get every line of the file
+	while(!fileStream.eof())
+	{
+		std::getline(fileStream, line);
+		content.append(line + "\n");
+	}
 
-  // return a string with the files contents
-  fileStream.close();
-  return content;
+	// return a string with the files contents
+	fileStream.close();
+	return content;
 }
 
 void ShaderFactory::LoadShaderFromFile(char* vertexLocation, char* fragmentLocation)
 {
-  this->LoadShader(loadFileToString(vertexLocation), loadFileToString(fragmentLocation));
+	this->LoadShader(loadFileToString(vertexLocation), loadFileToString(fragmentLocation));
 }
 
 void ShaderFactory::LoadShader(std::string vertexSource, std::string fragmentSource)
 {
-  //Convert the vertexShader string to a c-string and print it to console
-  const char* vertexChars = vertexSource.c_str();
-  std::cout << vertexChars << std::endl;
-
-  //Convert the fragmentShader string to a c-string and print it to console
-  const char* fragmentChars = fragmentSource.c_str();
-  std::cout << fragmentChars << std::endl;
-
-  //Create a vertex shader
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-  //Give the shader the proper source code and compile it
-  glShaderSource(vertexShader, 1, &vertexChars, NULL);
-  glCompileShader(vertexShader);
-
-  //Print out whether or not the shader compiled successfuly!
-  GLint status;
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-  std::cout << status << " Vertex Shader\n";
-
-  //Create a fragment shader
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-  //Give the shader the proper source code and compile it
-  glShaderSource(fragmentShader, 1, &fragmentChars, NULL);
-  glCompileShader(fragmentShader);
+	//Convert the vertexShader string to a c-string and print it to console
+	const char* vertexChars = vertexSource.c_str();
+	std::cout << vertexChars << "\n\n";
 
 
-  //Print out whether or not the shader compiled successfuly!
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-  std::cout << status << " Fragment Shader\n";
+	//Create a vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-  //Create a full shader program and attach the vertex and fragment shaders to it
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
+	//Give the shader the proper source code and compile it
+	glShaderSource(vertexShader, 1, &vertexChars, NULL);
+	glCompileShader(vertexShader);
 
-  //Link the shader program together and add it to the vector of loaded shaders
-  glLinkProgram(shaderProgram);
-  shaderPrograms.push_back(shaderProgram);
+	//Print out whether or not the shader compiled successfuly!
+	GLint status;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	std::cout << status << " Vertex Shader\n\n";
+
+	GLint logLen;
+	char* shaderLog;
+	if(!status)
+	{
+		glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &logLen);
+
+		shaderLog = new char[logLen+1];
+		glGetShaderInfoLog(vertexShader, logLen, NULL, shaderLog);
+		std::cout << "Shader Error Log:\n" << shaderLog << "\n\n";
+		delete shaderLog;
+	}
+
+
+	//Convert the fragmentShader string to a c-string and print it to console
+	const char* fragmentChars = fragmentSource.c_str();
+	std::cout << fragmentChars << "\n\n";
+	//Create a fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//Give the shader the proper source code and compile it
+	glShaderSource(fragmentShader, 1, &fragmentChars, NULL);
+	glCompileShader(fragmentShader);
+
+
+	//Print out whether or not the shader compiled successfuly!
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	std::cout << status << " Fragment Shader\n\n";
+
+	if(!status)
+	{
+		glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &logLen);
+
+		shaderLog = new char[logLen+1];
+		glGetShaderInfoLog(fragmentShader, logLen, NULL, shaderLog);
+		std::cout << "Shader Error Log:\n" << shaderLog << "\n" << std::endl;
+		delete shaderLog;
+	}
+	//Create a full shader program and attach the vertex and fragment shaders to it
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	//Link the shader program together and add it to the vector of loaded shaders
+	glLinkProgram(shaderProgram);
+	shaderPrograms.push_back(shaderProgram);
 }
