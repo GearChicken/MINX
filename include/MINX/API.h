df2ce84248d968e3367a35f2a7c3abbdf403ff2e
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-06-27 01:05:17 -0400

    Updated API.h to remove redefinition

--- include/MINX/API.h
+++ include/MINX/API.h
@@ -1,12 +1,15 @@
 #ifndef API_H_
 #define API_H_
 
+
 #ifdef _WIN32
 #ifdef _WINDLL
 #define MINX_API __declspec(dllexport)
 #else
 #define MINX_API __declspec(dllimport)
 #endif
+#else
+#define MINX_API
 #endif
 
 #endif
