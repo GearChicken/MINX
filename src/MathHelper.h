9ccd5c6d7354948eefe2e9f7b848f0e4db7bb131
heroyi <heroyisunshin@yahoo.com>
2014-07-01 08:35:46 -0700

    Update MathHelper.h
    added random member

--- src/MathHelper.h
+++ src/MathHelper.h
@@ -76,6 +76,7 @@ namespace MINX
 		extern MINX_API double clamp(double number, double lowerLimit, double upperLimit);
 		extern MINX_API double max(double num1, double num2);
 		extern MINX_API double min(double num1, double num2);
+		extern MINX_API double random(double x);
 	}
 }
 #endif
